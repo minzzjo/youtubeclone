9efd01a6d3dd11c6275a3f8ad44357277b4c1282
minzzjo <minxxjozz@gmail.com>
2024-05-22 23:48:55 +0900

    Editing VideoDetails

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 
 export default function VideoDetail() {
-	return <div>Video Detail🗃️</div>;
+	// FIXME: useLocation에서 state가 계속 null값으로 나옴
+	const {
+		state: { videos },
+	} = useLocation();
+	const { title, channelId, channelTitle, description } = videos.snippet;
+
+	return (
+		<div>
+			<article>
+				<iframe id='player' type='text/html' width='100%' height='640' src={`http://www.youtube.com/embed/${videos.id}`} frameborder='0' title={`${videos.title}`} />
+				<section>
+					<p>{title}</p>
+					<p>{channelTitle}</p>
+					<p>{description}</p>
+				</section>
+			</article>
+		</div>
+	);
 }
