49eb5209f6a7c67b6457620424e7540bafedfc08
minzzjo <minxxjozz@gmail.com>
2024-05-23 17:53:04 +0900

    Ready to Deploy

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,11 +1,9 @@
 import { createContext, useContext } from 'react';
-import Fake from '../apis/Fake';
 import Apis from '../apis/Apis';
 import Client from '../apis/Client';
 
 export const ApiContext = createContext();
 
-// const client = new Fake();
 const client = new Client();
 const youtube = new Apis(client);
 
